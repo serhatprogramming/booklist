1b075f8490574969fa64872552f49553e03e3fef
serhatprogramming <serhatprogramming@gmail.com>
2022-08-10 19:08:26 -0400

    Add Book updated

---
+++ app.js
@@ -0,0 +1,53 @@
+// Book Constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+// UI Constructor
+function UI() {}
+
+// Add Book To List
+UI.prototype.addBookToList = function (book) {
+  const list = document.querySelector("#book-list");
+
+  // create a row
+  const row = document.createElement("tr");
+
+  // add book to the row element
+  row.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.isbn}</td>
+  <td><a href="#" class = "delete-book">X</a></td>
+  `;
+  list.appendChild(row);
+};
+
+// Clear Fields
+UI.prototype.clearFields = function () {
+  document.querySelector("#title").value = "";
+  document.querySelector("#author").value = "";
+  document.querySelector("#isbn").value = "";
+};
+
+// Event Listeners
+document.querySelector("#book-form").addEventListener("submit", function (e) {
+  // Get form values
+  const title = document.querySelector("#title").value,
+    author = document.querySelector("#author").value,
+    isbn = document.querySelector("#isbn").value;
+  // Instantiate a book
+  const book = new Book(title, author, isbn);
+
+  // instantiate UI
+  const ui = new UI();
+
+  // Add book to list
+  ui.addBookToList(book);
+
+  // Clear the fields
+  ui.clearFields();
+
+  e.preventDefault();
+});
